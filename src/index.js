fb4ae3367238c067facd02461d18599e41662052
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-25 17:21:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,14 +10,19 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends Component {
-
+  const paragraph1 = React.createElement('p',{},'Two grannies having the time of their life!');
+  const paragraph1 = React.createElement('p',{},'Passengers:');
+  const listOf = React.createElement('ul',{},[
+    React.createElement('li',{},'Agnes'),
+    React.createElement('li',{},'Muriel')
+  ])
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
+   render() {
+     return (
+       React.createElement('div',{className:'oldercoaster'},)
+     )
+   }
+  
 }
 
 export class InFrontOfYou extends Component {
